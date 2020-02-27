b363f762b474f9496236c95f4ab4b4da7e85dfa9
metaleap <metaleap@users.noreply.github.com>
2020-02-27 19:37:11 +0100

    build-on-save: now with progress-report in status-bar (or whatever UX is in the LSP client)

--- wiplangserver/src_diags.zig
+++ wiplangserver/src_diags.zig
@@ -1,15 +1,5 @@
 usingnamespace @import("./_usingnamespace.zig");
 
-var build_progress_ongoing = false;
-
-pub fn onBuildRuns(situation: SrcFiles.OnBuildRuns) void {
-    switch (situation) {
-        .begun => {},
-        .ended => {},
-        .cur_build_dir => |build_dir_path| {},
-    }
-}
-
 pub fn onIssuePosCalc(mem_temp: *std.heap.ArenaAllocator, src: Str, tok_pos: [2]usize, kind: SrcFiles.Issue.PosInfoKind) ![]usize {
     const range = switch (kind) {
         .byte_offsets_0_based_range => try Range.initFromResliced(src, tok_pos[0], tok_pos[1]),
@@ -53,5 +43,36 @@ pub fn onFreshIssuesToPublish(mem_temp: *std.heap.ArenaAllocator, issues: std.St
 
         for (payloads.items[0..payloads.len]) |_, i|
             try server.api.notify(.textDocument_publishDiagnostics, payloads.items[i]);
+    }
+}
+
+var build_progress_token = ProgressToken{ .int = 0 };
+
+pub fn onBuildRuns(situation: SrcFiles.OnBuildRuns) void {
+    const lock = server.mutex.acquire();
+    defer lock.release();
+    switch (situation) {
+        .begun => {
+            build_progress_token.int = @intCast(i64, std.time.milliTimestamp());
+            server.api.request(.window_workDoneProgress_create, {}, WorkDoneProgressCreateParams{ .token = build_progress_token }, struct {
+                pub fn then(state: void, resp: Server.Ctx(Result(void))) error{}!void {}
+            }) catch return;
+            server.api.notify(.__progress, ProgressParams{
+                .token = build_progress_token,
+                .value = WorkDoneProgress{ .kind = "begin", .title = "zig build" },
+            }) catch return;
+        },
+        .ended => {
+            server.api.notify(.__progress, ProgressParams{
+                .token = build_progress_token,
+                .value = WorkDoneProgress{ .kind = "end", .title = "zig build" },
+            }) catch return;
+        },
+        .cur_build_dir => |build_dir_path| {
+            server.api.notify(.__progress, ProgressParams{
+                .token = build_progress_token,
+                .value = WorkDoneProgress{ .kind = "report", .title = "zig build", .message = build_dir_path },
+            }) catch return;
+        },
     }
 }
