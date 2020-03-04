a333fd07eb777c001565a1fcac9c2748c11591b1
metaleap <metaleap@users.noreply.github.com>
2020-03-04 20:37:05 +0100

    doc-comments hover (cur-file only for now)

--- wiplangserver/src_intel_hover.zig
+++ wiplangserver/src_intel_hover.zig
@@ -3,6 +3,7 @@ usingnamespace @import("./_usingnamespace.zig");
 pub fn onHover(ctx: Server.Ctx(HoverParams)) !Result(?Hover) {
     const src_file_abs_path = lspUriToFilePath(ctx.value.TextDocumentPositionParams.textDocument.uri);
     var markdowns = try std.ArrayList(Str).initCapacity(ctx.mem, 4);
+
     if (try zsess.src_intel.resolve(ctx.memArena(), .{
         .full_path = src_file_abs_path,
         .pos_info = &[2]usize{
@@ -11,31 +12,43 @@ pub fn onHover(ctx: Server.Ctx(HoverParams)) !Result(?Hover) {
         },
     })) |locked| {
         defer locked.held.release();
-        try markdowns.append(try std.fmt.allocPrint(ctx.mem, "{}", .{locked.item.node.id}));
         for (locked.item.resolveds) |resolved|
-            try markdowns.append(try toMarkDown(ctx.memArena(), resolved));
+            try markdowns.append(try toMarkDown(ctx.memArena(), &locked.item, resolved));
+        if (markdowns.len == 0)
+            try markdowns.append(try std.fmt.allocPrint(ctx.mem, "{}", .{locked.item.node.id}));
     }
 
     return Result(?Hover){
         .ok = Hover{
             .contents = MarkupContent{
                 .value = try std.mem.
-                    join(ctx.mem, "\n\n____\n\n", markdowns.toSliceConst()),
+                    join(ctx.mem, "\n \n____\n \n", markdowns.toSliceConst()),
             },
         },
     };
 }
 
-fn toMarkDown(mem: *std.heap.ArenaAllocator, resolved: SrcIntel.AstResolved) !Str {
-    switch (resolved) {
+/// foo __doc-comments__ go _here_,
+/// one more line,
+///
+/// and another one,
+/// for a quick `hover` try-out!
+fn toMarkDown(mem: *std.heap.ArenaAllocator, context: *const SrcIntel.Resolved, cur_resolved: SrcIntel.AstResolved) !Str {
+    switch (cur_resolved) {
         else => return try std.fmt.allocPrint(&mem.
-            allocator, "no toMarkDown impl yet for `{}`", .{std.meta.activeTag(resolved)}),
+            allocator, "no toMarkDown impl yet for `{}`", .{std.meta.activeTag(cur_resolved)}),
+
         .err_or_warning => |issue| return try std.fmt.allocPrint(&mem.
             allocator, "Problem with this `{}`:\n\n{}", .{ zag.mem.
             trimPrefix(u8, try std.fmt.allocPrint(&mem.allocator, "{}", .{issue.node_id}), "Id."), issue.detail }),
-        .boolean => return "```zig\nbool\n```\n"[0..],
+
+        .array => |arr| return try std.fmt.allocPrint(&mem.allocator, "{} array item(s)", .{arr.len}),
+
+        .boolean => |b| return try std.fmt.allocPrint(&mem.allocator, "```zig\n{}: bool\n```\n", .{b}),
+
         .string => |str| return try std.fmt.allocPrint(&mem.
             allocator, "- {} byte(s) ({} ASCII)\n- {} UTF-8 rune(s)\n- {} line-break(s)\n- {} tab-stop(s)", .{ str.len, zag.util.asciiByteCount(str), if (zag.util.utf8RuneCount(str)) |n| n else |_| 0, zag.mem.count(str, '\n'), zag.mem.count(str, '\t') }),
+
         .float => |float| {
             var str: Str = "";
             const format_chars = "{d}{e}";
@@ -46,18 +59,33 @@ fn toMarkDown(mem: *std.heap.ArenaAllocator, resolved: SrcIntel.AstResolved) !St
             }
             return str;
         },
-        .uint => |uint| {
+
+        .int => |int| {
             var str: Str = "";
             const format_chars = "{d}{x}{b}";
             comptime var i: usize = 0;
             inline while (i < format_chars.len) : (i += 3) {
-                const fmt_preview = try std.fmt.allocPrint(&mem.allocator, format_chars[i .. i + 3], .{uint});
+                const fmt_preview = try std.fmt.allocPrint(&mem.allocator, format_chars[i .. i + 3], .{int});
                 str = try std.fmt.allocPrint(&mem.allocator, "{s}- `{s}` &rarr; `{s}`\n", .{ str, format_chars[i .. i + 3], fmt_preview });
             }
-            if (uint > 32 and uint < 127) {
-                const fmt_preview = try std.fmt.allocPrint(&mem.allocator, "{c}", .{@intCast(u8, uint)});
+            if (int > 32 and int < 127) {
+                const fmt_preview = try std.fmt.allocPrint(&mem.allocator, "{c}", .{@intCast(u8, int)});
                 str = try std.fmt.allocPrint(&mem.allocator, "{s}- `{s}` &rarr; `{s}`\n", .{ str, "{c}", fmt_preview });
             }
+            return str;
+        },
+
+        .loc_ref_same_src_file => |node| {
+            const start = context.ast.tokens.at(node.firstToken()).start;
+            const end = context.ast.tokens.at(node.lastToken()).end;
+            var str: Str = "";
+            if (try SrcIntel.astNodeDocComments(mem, context.ast, node)) |doc_comment_lines|
+                if (doc_comment_lines.len != 0) {
+                    for (doc_comment_lines) |doc_comment_line|
+                        str = try std.fmt.allocPrint(&mem.allocator, "{s}{s}\n", .{ str, doc_comment_line });
+                    str = try std.fmt.allocPrint(&mem.allocator, "{s}\n \n____\n \n", .{str});
+                };
+            str = try std.fmt.allocPrint(&mem.allocator, "{s}```zig\n{}\n```\n", .{ str, context.ast.source[start..end] });
             return str;
         },
     }
